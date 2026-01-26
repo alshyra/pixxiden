5e37a39888eafccab533ccdd6e0c5c73369ae3ec
Antoine Savajols <a.savajols.ext@sinari.com>
2026-01-26 08:56:57 +0100

    code mort

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,4 +1,3 @@
-export { default as TopBar } from './TopBar.vue'
 export { default as BottomFilters } from './BottomFilters.vue'
 export { default as FilterButton } from './FilterButton.vue'
 export { default as ConsoleFooter } from './ConsoleFooter.vue'
